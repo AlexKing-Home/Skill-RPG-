99d551c29bac4a8760e1ffeee2db233fb48fd7c9
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-14 07:34:51 +0300

    fix: restore female swordswoman portrait

--- src/data/assets.js
+++ src/data/assets.js
@@ -1,4 +1,4 @@
-const asset = (path) => `${path}?v=clean-assets-v3`;
+const asset = (path) => `${path}?v=clean-assets-v4`;
 
 export const fantasyBackground = asset("/backgrounds/welcome-bg.jpg");
 
@@ -6,7 +6,7 @@ export const maleSwordsman = asset("/skins/male-swordsman.jpg");
 export const maleSpearman = asset("/skins/male-spearman.jpg");
 export const maleAssassin = asset("/skins/male-assassin.jpg");
 export const maleArcher = asset("/skins/male-archer.jpg");
-export const femaleSwordsman = asset("/skins/female-swordsman.svg");
+export const femaleSwordsman = asset("/skins/female-swordsman.jpg");
 export const femaleSpearman = asset("/skins/female-spearman.avif");
 export const femaleAssassin = asset("/skins/female-assassin.avif");
 export const femaleArcher = asset("/skins/female-archer.avif");
