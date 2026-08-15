d4fc1da50df2d6e6658e6de9bd8a4ec81bc50b92
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-15 18:02:27 +0300

    fix: load mobile parity layer last

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./welcome-background.css";
+import "./parity-fixes.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
