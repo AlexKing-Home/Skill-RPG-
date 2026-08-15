0d413b4ef7849b3ddfc4d8d32eb32e0817fc5ed8
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-15 22:15:00 +0300

    asset: assemble floor one map art

---
+++ src/data/floorOneMapArt.js
@@ -0,0 +1,10 @@
+import c1 from "./mapChunks/floor1-01.js";
+import c2 from "./mapChunks/floor1-02.js";
+import c3 from "./mapChunks/floor1-03.js";
+import c4 from "./mapChunks/floor1-04.js";
+import c5 from "./mapChunks/floor1-05.js";
+import c6 from "./mapChunks/floor1-06.js";
+import c7 from "./mapChunks/floor1-07.js";
+import c8 from "./mapChunks/floor1-08.js";
+
+export const floorOneMapArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}${c7}${c8}`;
