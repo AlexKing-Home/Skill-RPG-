f8ab3a3c194e10cc63360d7095b380af0e4aaba6
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-16 00:09:22 +0300

    asset: assemble approved swamp location art

---
+++ src/data/swampLocationArt.js
@@ -0,0 +1,8 @@
+import c1 from "./swampLocationChunks/swamp-01.js";
+import c2 from "./swampLocationChunks/swamp-02.js";
+import c3 from "./swampLocationChunks/swamp-03.js";
+import c4 from "./swampLocationChunks/swamp-04.js";
+import c5 from "./swampLocationChunks/swamp-05.js";
+import c6 from "./swampLocationChunks/swamp-06.js";
+
+export const swampLocationArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}`;
