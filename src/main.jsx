dd0099856fc74c94f211cda2dc0cdf2de26b7be8
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-15 21:07:42 +0300

    style: load refined navigation parity

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import "./device-regression-fixes.css";
 import "./reference-parity-final.css";
 import "./mobile-polish.css";
 import "./reference-ui-v8.css";
-import "./navigation-reference-v9.css";
+import "./navigation-reference.css";
+import "./navigation-reference-v2.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
