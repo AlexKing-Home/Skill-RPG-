fc6e429c6ff692064c10ae95515411b999861383
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-16 13:08:09 +0300

    feat: assemble approved meadow location artwork

---
+++ src/data/meadowLocationArt.js
@@ -0,0 +1,8 @@
+import meadow1 from "./meadowLocationChunks/meadow-01.js";
+import meadow2 from "./meadowLocationChunks/meadow-02.js";
+import meadow3 from "./meadowLocationChunks/meadow-03.js";
+import meadow4 from "./meadowLocationChunks/meadow-04.js";
+import meadow5 from "./meadowLocationChunks/meadow-05.js";
+import meadow6 from "./meadowLocationChunks/meadow-06.js";
+
+export const meadowLocationArt = `data:image/webp;base64,${meadow1}${meadow2}${meadow3}${meadow4}${meadow5}${meadow6}`;
