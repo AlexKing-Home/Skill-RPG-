e07f2e0d7800ea6448fded8927b5d28dd1fcb26f
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-16 10:23:14 +0300

    fix: embed real swamp sublocation art

--- src/data/swampSubLocations.js
+++ src/data/swampSubLocations.js
@@ -1,4 +1,7 @@
-import { swampDeepPathArt } from "./swampDeepPathArt.js";
-import { swampRootsBurrowArt } from "./swampRootsBurrowArt.js";
+import deep1 from "./swampSubLocationChunks/deep-01.js";
+import deep2 from "./swampSubLocationChunks/deep-02.js";
+import roots1 from "./swampSubLocationChunks/roots-01.js";
+import roots2 from "./swampSubLocationChunks/roots-02.js";
 
-export { swampDeepPathArt, swampRootsBurrowArt };
+export const swampDeepPathArt = `data:image/webp;base64,${deep1}${deep2}`;
+export const swampRootsBurrowArt = `data:image/webp;base64,${roots1}${roots2}`;
