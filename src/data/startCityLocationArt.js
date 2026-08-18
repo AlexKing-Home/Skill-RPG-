01ad3fe52b45d6bdc48eba959d50d2efe0bb5c31
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-18 19:13:05 +0300

    fix: use approved start city art

--- src/data/startCityLocationArt.js
+++ src/data/startCityLocationArt.js
@@ -1,10 +1,12 @@
-import city1 from "./startCityLocationChunksV2/city-v2-01.js";
-import city2 from "./startCityLocationChunksV2/city-v2-02.js";
-import city3 from "./startCityLocationChunksV2/city-v2-03.js";
-import city4 from "./startCityLocationChunksV2/city-v2-04.js";
-import city5 from "./startCityLocationChunksV2/city-v2-05.js";
-import city6 from "./startCityLocationChunksV2/city-v2-06.js";
-import city7 from "./startCityLocationChunksV2/city-v2-07.js";
-import city8 from "./startCityLocationChunksV2/city-v2-08.js";
+import city1 from "./startCityApprovedWebp/city-01.js";
+import city2 from "./startCityApprovedWebp/city-02.js";
+import city3 from "./startCityApprovedWebp/city-03.js";
+import city4 from "./startCityApprovedWebp/city-04.js";
+import city5 from "./startCityApprovedWebp/city-05.js";
+import city6 from "./startCityApprovedWebp/city-06.js";
+import city7 from "./startCityApprovedWebp/city-07.js";
+import city8 from "./startCityApprovedWebp/city-08.js";
+import city9 from "./startCityApprovedWebp/city-09.js";
 
-export const startCityLocationArt = `data:image/webp;base64,${city1}${city2}${city3}${city4}${city5}${city6}${city7}${city8}`;
+export const startCityLocationArt =
+  `data:image/webp;base64,${city1}${city2}${city3}${city4}${city5}${city6}${city7}${city8}${city9}`;
