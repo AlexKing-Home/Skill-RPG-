f64be4736e99896d0994eeeedb0b9fa75205c720
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-18 11:14:02 +0300

    fix: use complete start city art payload

--- src/data/startCityLocationArt.js
+++ src/data/startCityLocationArt.js
@@ -1,6 +1,11 @@
-import city1 from "./startCityLocationChunks/city-01.js";
-import city2 from "./startCityLocationChunks/city-02.js";
-import city3 from "./startCityLocationChunks/city-03.js";
-import city4 from "./startCityLocationChunks/city-04.js";
+import city1 from "./startCityLocationChunksV2/city-v2-01.js";
+import city2 from "./startCityLocationChunksV2/city-v2-02.js";
+import city3 from "./startCityLocationChunksV2/city-v2-03.js";
+import city4 from "./startCityLocationChunksV2/city-v2-04.js";
+import city5 from "./startCityLocationChunksV2/city-v2-05.js";
+import city6 from "./startCityLocationChunksV2/city-v2-06.js";
+import city7 from "./startCityLocationChunksV2/city-v2-07.js";
+import city8 from "./startCityLocationChunksV2/city-v2-08.js";
 
-export const startCityLocationArt = `data:image/webp;base64,${city1}${city2}${city3}${city4}`;
+export const startCityLocationArt =
+  `data:image/webp;base64,${city1}${city2}${city3}${city4}${city5}${city6}${city7}${city8}`;
