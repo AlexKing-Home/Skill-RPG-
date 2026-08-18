7c53865170d9fae05e3543404485d667a777e473
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-18 22:37:11 +0300

    assets: prepare dungeon artwork

---
+++ src/data/dungeonLocationArt.js
@@ -0,0 +1,8 @@
+import c1 from "./dungeonLocationChunks/dungeon-01.js";
+import c2 from "./dungeonLocationChunks/dungeon-02.js";
+import c3 from "./dungeonLocationChunks/dungeon-03.js";
+import c4 from "./dungeonLocationChunks/dungeon-04.js";
+import c5 from "./dungeonLocationChunks/dungeon-05.js";
+import c6 from "./dungeonLocationChunks/dungeon-06.js";
+
+export const dungeonLocationArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}`;
