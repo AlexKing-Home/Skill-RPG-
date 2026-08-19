c6b1cb501ef85436f836adbee8f81f6b66401c7e
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-19 07:19:38 +0300

    feat: assemble forest artwork like other locations

---
+++ src/data/forestLocationArt.js
@@ -0,0 +1,10 @@
+import c1 from "./forestLocationChunks/forest-01.js";
+import c2 from "./forestLocationChunks/forest-02.js";
+import c3 from "./forestLocationChunks/forest-03.js";
+import c4 from "./forestLocationChunks/forest-04.js";
+import c5 from "./forestLocationChunks/forest-05.js";
+import c6 from "./forestLocationChunks/forest-06.js";
+import c7 from "./forestLocationChunks/forest-07.js";
+import c8 from "./forestLocationChunks/forest-08.js";
+
+export const forestLocationArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}${c7}${c8}`;
