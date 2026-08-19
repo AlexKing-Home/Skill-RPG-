c90a9c10b1baa3257d4ee3eb5b5e43234e6892c8
AlexKing-Home <alexeykotovalex@gmail.com>
2026-08-19 07:31:54 +0300

    fix: rebuild forest artwork from corrected chunks

--- src/data/forestLocationArt.js
+++ src/data/forestLocationArt.js
@@ -3,8 +3,9 @@ import c2 from "./forestLocationChunks/forest-02.js";
 import c3 from "./forestLocationChunks/forest-03.js";
 import c4 from "./forestLocationChunks/forest-04.js";
 import c5 from "./forestLocationChunks/forest-05.js";
-import c6 from "./forestLocationChunks/forest-06.js";
+import c6a from "./forestLocationChunks/forest-06a.js";
+import c6b from "./forestLocationChunks/forest-06b.js";
 import c7 from "./forestLocationChunks/forest-07.js";
 import c8 from "./forestLocationChunks/forest-08.js";
 
-export const forestLocationArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}${c7}${c8}`;
+export const forestLocationArt = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6a}${c6b}${c7}${c8}`;
